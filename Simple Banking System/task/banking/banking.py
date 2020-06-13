2c5253ac6c3e2972ce29e55f1fde4bfa949713b9
Alexandr Pahomov <gxtrus@gmail.com>
2020-06-13 21:48:22 +0300

    Stage #4: Advanced system
    Description
    
    You have created the foundation of our banking system. Now let's take the opportunity to deposit money into an account, make transfers and close an account if necessary.
    
    Now your menu should look like this:
    
    1. Balance
    2. Add income
    3. Do transfer
    4. Close account
    5. Log out
    0. Exit
    If the user asks for Balance, you should read the balance of the account from the database and output it into the console.
    
    Add income item should allow us to deposit money to the account.
    
    Do transfer item should allow to transfer money to another account. You should handle the following errors:
    
    If the user tries to transfer money to the same account, output the following message: “You can't transfer money to the same account!”
    If the receiver's card number doesn’t pass the Luhn algorithm, you should output: “Probably you made mistake in card number. Please try again!”
    If the receiver's card number doesn’t exist, you should output: “Such a card does not exist.”
    If there is no error, ask the user how much money they want to transfer and make the transaction.
    If the user chooses the Close an account item, you should delete that account from database.

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,6 +1,6 @@
 import random
 
-from sqlalchemy import Column, Integer, String, create_engine, Text
+from sqlalchemy import Column, Integer, Text, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -54,7 +54,7 @@ class Bank:
         random_number = random.randint(0, 999999999)
         card_number = f'400000{random_number:09d}'
         last_number = self.luhn_algorithm(card_number)
-        card_number += str(last_number)
+        card_number += last_number
         card_pin = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(
             random.randint(0, 9))
         new_row = self.CardTable(number=card_number, pin=card_pin)
@@ -68,26 +68,43 @@ class Bank:
         return
 
     def luhn_algorithm(self, card_number):
-        card_number_list = list(card_number)
+        card_number_list = list(card_number[0:15])
         for x in range(0, 16, 2):
             y = int(card_number_list[x]) * 2
             card_number_list[x] = str(y - 9 if y > 9 else y)
         sum = 0
         for x in card_number_list:
             sum += int(x)
-        return (10 - sum % 10) % 10
+        return str((10 - sum % 10) % 10)
 
     def work_with_account(self):
         card_info = self.login_account()
         result = ''
         if card_info:
             while True:
-                print('1. Balance\n2. Log out\n0. Exit')
-                user_input = self.enter_choice(('0', '1', '2'))
+                print('1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit')
+                user_input = self.enter_choice(('0', '1', '2', '3', '4', '5'))
+                # 1 Balance
                 if user_input == '1':
                     balance = card_info[0].balance
                     print(f'\nBalance: {balance}\n')
+                # 2 Add income
                 elif user_input == '2':
+                    income = int(input('\nEnter income\n'))
+                    card_info[0].balance += income
+                    self.session.commit()
+                    print('Income was added!\n')
+                # 3 Do transfer
+                elif user_input == '3':
+                    self.transfer(card_info)
+                # 4 Close account
+                elif user_input == '4':
+                    self.session.delete(card_info[0])
+                    self.session.commit()
+                    print('\nThe account has been closed!\n')
+                    break
+                # 5 Log out
+                elif user_input == '5':
                     print('\nYou have successfully logged out!\n')
                     break
                 else:
@@ -100,16 +117,36 @@ class Bank:
         card_number = str(input().strip())
         print('Enter your PIN:')
         card_pin = str(input().strip())
-        # rows = self.session.query(self.CardTable).all()
-        # print(rows)
         rows = self.session.query(self.CardTable).filter(self.CardTable.number == card_number).all()
-        # print(rows)
         if rows == [] or (rows[0].pin != card_pin):
             print('\nWrong card number or PIN!\n')
             return False
         else:
             print('\nYou have successfully logged in!\n')
             return rows
+
+    def transfer(self, card_info):
+        transfer_card = input('\nTransfer\nEnter card number:\n')
+        last_number = self.luhn_algorithm(transfer_card[0:15])
+        rows = self.session.query(self.CardTable).filter(self.CardTable.number == transfer_card).all()
+        if card_info[0].number == transfer_card:
+            print("You can't transfer money to the same account!\n")
+            return False
+        elif transfer_card[-1] != last_number:
+            print('\nProbably you made mistake in the card number. Please try again!\n')
+            return False
+        elif rows == []:
+            print('Such a card does not exist.\n')
+            return False
+        money = int(input('Enter how much money you want to transfer:\n'))
+        if money > card_info[0].balance:
+            print('Not enough money!\n')
+            return False
+        rows[0].balance += money
+        card_info[0].balance -= money
+        self.session.commit()
+        print('Success!\n')
+        return True
 
 
 if __name__ == '__main__':
